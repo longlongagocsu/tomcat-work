5e1bc0b40fca919022c777dc09d7ae7a1e2e42ec
longlongagocsu <donglongjun886@qq.com>
2017-12-15 14:52:47 +0800

    add:添加HttpServer2

---
+++ src/main/java/ex02/pyrmont/HttpServer2.java
@@ -0,0 +1,72 @@
+package ex02.pyrmont;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+/**
+* @author 董龙君
+* @date 创建时间: 2017年12月13日 下午5:45:05
+**/
+
+public class HttpServer2 {
+	
+	public static final String WEB_ROOT=System.getProperty("user.dir")+File.separator+
+			"src"+File.separator+"main"+File.separator+"webapp";
+	
+	private static final String SHUTDOWN_COMMAND="/SHUTDOWN";
+	
+	private boolean shutdown=false;
+
+	public static void main(String[] args) throws Exception {
+		HttpServer2 server=new HttpServer2();
+		server.await();
+	}
+	
+	public void await() {
+		ServerSocket serverSocket=null;
+		int port=8080;
+		try {
+			serverSocket=new ServerSocket(port,10,InetAddress.getByName("127.0.0.1"));
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.exit(1);
+		}
+		while(!shutdown) {
+			Socket socket=null;
+			InputStream input=null;
+			OutputStream output=null;
+			try {
+				socket=serverSocket.accept();
+				input=socket.getInputStream();
+				output=socket.getOutputStream();
+				Request request=new Request(input);
+				request.parse();
+				Response response=new Response(output);
+				response.setRequest(request);
+				if(request.getUri().startsWith("/servlet/")) {
+					ServletProcessor2 processor=new ServletProcessor2();
+					processor.process(request, response);
+				}else {
+					StaticResourceProcessor processor=new StaticResourceProcessor();
+					processor.processor(request, response);
+				}
+				socket.close();
+				shutdown=request.getUri().equals(SHUTDOWN_COMMAND);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
---
+++ src/main/java/ex02/pyrmont/ServletProcessor2.java
@@ -0,0 +1,56 @@
+package ex02.pyrmont;
+
+import java.io.File;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.net.URLStreamHandler;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+/**
+* @author 董龙君
+* @date 创建时间: 2017年12月13日 下午5:49:20
+**/
+
+public class ServletProcessor2 {
+	
+	public void process(Request request,Response  response) {
+		String uri=request.getUri();
+		String servletName=uri.substring(uri.lastIndexOf("/")+1);
+		URLClassLoader loader=null;
+		try {
+			URL[] urls=new URL[1];
+			URLStreamHandler streamHandler=null;
+			File classPath=new File(this.getClass().getResource("").getPath());
+			String repository=(new URL("file",null,classPath.getCanonicalPath()+File.separator)).toString();
+			urls[0]=new URL(null,repository,streamHandler);
+			loader=new URLClassLoader(urls);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Class myClass=null;
+		try {
+			myClass=loader.loadClass("ex02.pyrmont."+servletName);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Servlet servlet=null;
+		RequestFacade requestFacade=new RequestFacade(request);
+		ResponseFacade responseFacade=new ResponseFacade(response);
+		try {
+			servlet=(Servlet)myClass.newInstance();
+			servlet.service((ServletRequest)requestFacade, (ServletResponse)responseFacade);
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
